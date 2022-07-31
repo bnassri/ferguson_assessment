51db17098f2ec60599cb8b9058bdea6cbd30c703
bnassri <bnassri@gmail.com>
2022-07-31 15:36:07 -0400

    Task 1 work

--- src/main/java/com/build/qa/build/selenium/pageobjects/Task1_SearchResultsPage.java
+++ src/main/java/com/build/qa/build/selenium/pageobjects/Task1_SearchResultsPage.java
@@ -2,18 +2,14 @@ package com.build.qa.build.selenium.pageobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import com.build.qa.build.selenium.pageobjects.BasePage;
 
-public class SearchResultsPage extends BasePage {
+public class Task1_SearchResultsPage extends BasePage {
     private By brandName;
     private By productNum;
 
-    public SearchResultsPage(WebDriver driver, Wait<WebDriver> wait) {
+    public Task1_SearchResultsPage(WebDriver driver, Wait<WebDriver> wait) {
         super(driver, wait);
         brandName = By.xpath("//h2[@class='product__brand']") ;
         productNum = By.xpath("//span[. = 'Part #M6702BN']");
